6d75742b5039b23c69e2cb90f00a3f28cca49aae
Ole Begemann <ole@oleb.net>
2013-03-20 17:54:11 +0100

    Create blank Command Line project in Xcode

---
+++ docopt/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  docopt
+//
+//  Created by Ole Begemann on 03/20/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+
+    return 0;
+}
