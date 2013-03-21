944e5bbb02c5f1db22be75fcd7805c01fdbf253d
Ole Begemann <ole@oleb.net>
2013-03-21 17:00:25 +0100

    Add Unit Testing Bundle

---
+++ DocoptTests/DocoptTests.h
@@ -0,0 +1,13 @@
+//
+//  DocoptTests.h
+//  DocoptTests
+//
+//  Created by Ole Begemann on 21.03.13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DocoptTests : SenTestCase
+
+@end
---
+++ DocoptTests/DocoptTests.m
@@ -0,0 +1,32 @@
+//
+//  DocoptTests.m
+//  DocoptTests
+//
+//  Created by Ole Begemann on 21.03.13.
+//
+//
+
+#import "DocoptTests.h"
+
+@implementation DocoptTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DocoptTests");
+}
+
+@end
