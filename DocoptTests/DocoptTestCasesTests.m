b3d6ca92381046a93609b4c523f98a27076d1c43
Ole Begemann <ole@oleb.net>
2013-05-07 15:49:42 +0200

    Add tests for existence and opening of fixtures file

--- DocoptTests/DocoptTestCasesTests.m
+++ DocoptTests/DocoptTestCasesTests.m
@@ -27,15 +27,42 @@
 {
 }
 
+- (void)testFixturesFileExists
+{
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    BOOL fileExists = [fileManager fileExistsAtPath:[self fixturesFilePath]];
+    STAssertTrue(fileExists, @"Fixtures file testcases.docopt does not exist in testing bundle");
+}
+
+- (void)testFixturesFileCanBeOpened
+{
+    STAssertNotNil([self fixturesFileContents], @"Could not read fixtures file");
+}
+
 - (void)testTestCases
 {
-    NSString *rawTestCases = [NSString stringWithContentsOfFile:@"/Users/elo/code/docopt/testcases.docopt" encoding:NSUTF8StringEncoding error:NULL];
+    NSString *rawTestCases = [self fixturesFileContents];
     self.parser = [[DocoptTestCaseParser alloc] initWithString:rawTestCases];
     
     for (DocoptTestCase *testCase in self.parser.testCases) {
         Docopt *docopt = [[Docopt alloc] initWithUsageDocumentation:testCase.usageDocumentation arguments:testCase.arguments];
         STAssertEqualObjects(docopt.result, testCase.expectedOutput, @"Test %@ failed", testCase.name);
     }
+}
+
+#pragma mark - Helper methods
+
+- (NSString *)fixturesFilePath
+{
+    NSBundle *testingBundle = [NSBundle bundleForClass:[self class]];
+    return [testingBundle pathForResource:@"testcases" ofType:@"docopt"];
+}
+
+- (NSString *)fixturesFileContents
+{
+    NSError *error = nil;
+    NSString *fileContents = [NSString stringWithContentsOfFile:[self fixturesFilePath] encoding:NSUTF8StringEncoding error:&error];
+    return fileContents;
 }
 
 @end
