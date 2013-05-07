f50c484b0d8816e209b07e23245ace31430b9e2c
Ole Begemann <ole@oleb.net>
2013-05-07 15:05:46 +0200

    Comment purpose of DocoptTestCasesTests class

--- DocoptTests/DocoptTestCasesTests.h
+++ DocoptTests/DocoptTestCasesTests.h
@@ -8,6 +8,11 @@
 
 #import <SenTestingKit/SenTestingKit.h>
 
+/*
+ *  This class parses the official docopt test cases file (testcases.docopt)
+ *  and runs the Docopt parser against the test cases defined in that file.
+ *  It issues one STAssert... failure for each failing test case.
+ */
 @interface DocoptTestCasesTests : SenTestCase
 
 @end
